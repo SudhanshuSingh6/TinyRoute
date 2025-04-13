c88148e54ab34e49c45fece8ab22d2b2cd81ca14
Sudhanshu Singh <sudhanshus026@gmail.com>
2025-04-13 13:59:01 +0530

    Initial commit

---
+++ src/main/java/com/tinyroute/TinyRouteApplication.java
@@ -0,0 +1,13 @@
+package com.tinyroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyRouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinyroute/TinyRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinyroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
