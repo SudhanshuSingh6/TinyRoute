9b85f34f663cf8ad8fa3d489d329497b725d242c
Sudhanshu Singh <sudhanshus026@gmail.com>
2025-04-22 23:58:43 +0530

    security config

--- src/main/java/com/tinyroute/service/UserDetailsServiceImpl.java
+++ src/main/java/com/tinyroute/service/UserDetailsServiceImpl.java
@@ -17,6 +17,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"+username));
-        return null;
+        return UserDetailsImpl.build(user);
     }
 }
