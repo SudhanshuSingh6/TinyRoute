3629b014e19b1e1862445ae9c32f9e65ddf4fa20
SudhanshuSingh6 <sudhanshus026@gmail.com>
2025-04-25 01:19:04 +0530

    move springboot files into backend folder and setting up frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return(
+    <>
+      <h1 class ="text-3xl font-semibold underline">
+       Hello World!
+      </h1>
+    </>
+  )
+}
+
+export default App
