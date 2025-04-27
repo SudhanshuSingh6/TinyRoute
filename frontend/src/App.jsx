0844792b264169579ac1ebc537dd8025f917bb4e
SudhanshuSingh6 <sudhanshus026@gmail.com>
2025-04-28 02:10:48 +0530

    setting up dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,18 +7,21 @@ import Footer from "./components/Footer";
 import RegisterPage from "./components/RegisterPage";
 import Login from "./components/Login";
 import { Toaster } from "react-hot-toast";
+import DashboardLayout from "./Dashboard/DashboardLayout";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        <Toaster position="bottom-center"/>
+        <Toaster position="bottom-center" />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DashboardLayout />} />
         </Routes>
         <Footer />
       </Router>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
+import { ContextProvider } from "./contextApi/ContextApi.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </StrictMode>
 );
