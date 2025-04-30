400424771be1d1bbb4ae86e07437ee06cde61633
SudhanshuSingh6 <sudhanshus026@gmail.com>
2025-04-30 19:15:34 +0530

    managing public and private routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import LandingPage from "./components/LandingPage";
-import AboutPage from "./components/AboutPage";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import RegisterPage from "./components/RegisterPage";
-import Login from "./components/Login";
-import { Toaster } from "react-hot-toast";
-import DashboardLayout from "./components/Dashboard/DashboardLayout";
+import { BrowserRouter as Router } from "react-router-dom";
+import { getApps } from "./utils/helper";
 
 function App() {
+  const CurrentApp = getApps();
+
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Toaster position="bottom-center" />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<DashboardLayout />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </>
+    <Router>
+      <CurrentApp />
+    </Router>
   );
 }
 
--- frontend/src/components/Dashboard/ShortenItem.jsx
+++ frontend/src/components/Dashboard/ShortenItem.jsx
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
 import React, { useEffect, useState } from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
 import { FaExternalLinkAlt, FaRegCalendarAlt } from "react-icons/fa";
 import { IoCopy } from "react-icons/io5";
 import { LiaCheckSolid } from "react-icons/lia";
@@ -7,6 +8,7 @@ import { MdAnalytics, MdOutlineAdsClick } from "react-icons/md";
 import api from "../../api/api";
 import { useNavigate } from "react-router-dom";
 import { useStoreContext } from "../../contextApi/ContextApi";
+import { Hourglass } from "react-loader-spinner";
 import Graph from "./Graph";
 
 const ShortenItem = ({ originalUrl, shortUrl, clickCount, createdDate }) => {
@@ -22,18 +24,6 @@ const ShortenItem = ({ originalUrl, shortUrl, clickCount, createdDate }) => {
     /^https?:\/\//,
     ""
   );
-
-  const handleCopy = async () => {
-    try {
-      await navigator.clipboard.writeText(
-        `${import.meta.env.VITE_REACT_SUBDOMAIN}/${shortUrl}`
-      );
-      setIsCopied(true);
-      setTimeout(() => setIsCopied(false), 1500);
-    } catch (err) {
-      console.error("Clipboard copy failed:", err);
-    }
-  };
 
   const analyticsHandler = (shortUrl) => {
     if (!analyticToggle) {
@@ -57,9 +47,10 @@ const ShortenItem = ({ originalUrl, shortUrl, clickCount, createdDate }) => {
       );
       setAnalyticsData(data);
       setSelectedUrl("");
+      console.log(data);
     } catch (error) {
       navigate("/error");
-      console.error(error);
+      console.log(error);
     } finally {
       setLoader(false);
     }
@@ -72,38 +63,45 @@ const ShortenItem = ({ originalUrl, shortUrl, clickCount, createdDate }) => {
   }, [selectedUrl]);
 
   return (
-    <div className="bg-slate-100 shadow-lg border border-dotted border-slate-500 px-6 sm:py-1 py-3 rounded-md transition-all duration-100">
-      <div className="flex sm:flex-row flex-col sm:justify-between w-full sm:gap-0 gap-5 py-5">
-        <div className="flex-1 sm:space-y-1 max-w-full overflow-x-auto overflow-y-hidden">
-          <div className="text-slate-900 pb-1 flex items-center gap-2">
+    <div
+      className={`bg-slate-100 shadow-lg border border-dotted  border-slate-500 px-6 sm:py-1 py-3 rounded-md  transition-all duration-100 `}
+    >
+      <div
+        className={`flex sm:flex-row flex-col  sm:justify-between w-full sm:gap-0 gap-5 py-5 `}
+      >
+        <div className="flex-1 sm:space-y-1 max-w-full overflow-x-auto overflow-y-hidden ">
+          <div className="text-slate-900 pb-1 sm:pb-0   flex items-center gap-2 ">
             <a
               href={`${import.meta.env.VITE_REACT_SUBDOMAIN}/${shortUrl}`}
               target="_blank"
-              rel="noopener noreferrer"
-              className="text-[17px] font-montserrat font-[600] text-linkColor"
+              className=" text-[17px]  font-montserrat font-[600] text-linkColor "
             >
-              {subDomain + "/" + shortUrl}
+              {subDomain + "/" + `${shortUrl}`}
             </a>
             <FaExternalLinkAlt className="text-linkColor" />
           </div>
 
-          <div className="flex items-center gap-1">
-            <h3 className="text-slate-700 font-[400] text-[17px]">
+          <div className="flex items-center gap-1 ">
+            <h3 className=" text-slate-700 font-[400] text-[17px] ">
               {originalUrl}
             </h3>
           </div>
 
-          <div className="flex items-center gap-8 pt-6">
-            <div className="flex gap-1 items-center font-semibold text-green-800">
-              <MdOutlineAdsClick className="text-[22px] me-1" />
+          <div className="flex   items-center gap-8 pt-6 ">
+            <div className="flex gap-1  items-center font-semibold  text-green-800">
+              <span>
+                <MdOutlineAdsClick className="text-[22px] me-1" />
+              </span>
               <span className="text-[16px]">{clickCount}</span>
-              <span className="text-[15px]">
-                {clickCount === 1 ? "Click" : "Clicks"}
+              <span className="text-[15px] ">
+                {clickCount === 0 || clickCount === 1 ? "Click" : "Clicks"}
               </span>
             </div>
 
             <div className="flex items-center gap-2 font-semibold text-lg text-slate-800">
-              <FaRegCalendarAlt />
+              <span>
+                <FaRegCalendarAlt />
+              </span>
               <span className="text-[17px]">
                 {dayjs(createdDate).format("MMM DD, YYYY")}
               </span>
@@ -111,57 +109,69 @@ const ShortenItem = ({ originalUrl, shortUrl, clickCount, createdDate }) => {
           </div>
         </div>
 
-        <div className="flex flex-1 sm:justify-end items-center gap-4">
-          <div
-            onClick={handleCopy}
-            className="flex cursor-pointer gap-1 items-center bg-btnColor py-2 font-semibold shadow-md shadow-slate-500 px-6 rounded-md text-white"
+        <div className="flex  flex-1  sm:justify-end items-center gap-4">
+          <CopyToClipboard
+            onCopy={() => setIsCopied(true)}
+            text={`${import.meta.env.VITE_REACT_SUBDOMAIN}/${shortUrl}`}
           >
-            <button>{isCopied ? "Copied" : "Copy"}</button>
-            {isCopied ? (
-              <LiaCheckSolid className="text-md" />
-            ) : (
-              <IoCopy className="text-md" />
-            )}
-          </div>
+            <div className="flex cursor-pointer gap-1 items-center bg-btnColor py-2  font-semibold shadow-md shadow-slate-500 px-6 rounded-md text-white ">
+              <button className="">{isCopied ? "Copied" : "Copy"}</button>
+              {isCopied ? (
+                <LiaCheckSolid className="text-md" />
+              ) : (
+                <IoCopy className="text-md" />
+              )}
+            </div>
+          </CopyToClipboard>
 
           <div
             onClick={() => analyticsHandler(shortUrl)}
-            className="flex cursor-pointer gap-1 items-center bg-rose-700 py-2 font-semibold shadow-md shadow-slate-500 px-6 rounded-md text-white"
+            className="flex cursor-pointer gap-1 items-center bg-rose-700 py-2 font-semibold shadow-md shadow-slate-500 px-6 rounded-md text-white "
           >
             <button>Analytics</button>
             <MdAnalytics className="text-md" />
           </div>
         </div>
       </div>
-
-      <div
-        className={`${
-          analyticToggle ? "flex" : "hidden"
-        } max-h-96 sm:mt-0 mt-5 min-h-96 relative border-t-2 w-full overflow-hidden`}
-      >
-        {loader ? (
-          <div className="min-h-[calc(450px-140px)] flex justify-center items-center w-full">
-            <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-blue-600"></div>
-            <p className="ml-3 text-slate-700">Please Wait...</p>
-          </div>
-        ) : (
-          <>
-            {analyticsData.length === 0 ? (
-              <div className="absolute flex flex-col justify-center sm:items-center items-end w-full left-0 top-0 bottom-0 right-0 m-auto">
-                <h1 className="text-slate-800 font-serif sm:text-2xl text-[15px] font-bold mb-1">
-                  No Data For This Time Period
-                </h1>
-                <h3 className="sm:w-96 w-[90%] sm:ml-0 pl-6 text-center sm:text-lg text-[12px] text-slate-600">
-                  Share your short link to view where your engagements are
-                  coming from
-                </h3>
+      <React.Fragment>
+        <div
+          className={`${
+            analyticToggle ? "flex" : "hidden"
+          }  max-h-96 sm:mt-0 mt-5 min-h-96 relative  border-t-2 w-[100%] overflow-hidden `}
+        >
+          {loader ? (
+            <div className="min-h-[calc(450px-140px)] flex justify-center items-center w-full">
+              <div className="flex flex-col items-center gap-1">
+                <Hourglass
+                  visible={true}
+                  height="50"
+                  width="50"
+                  ariaLabel="hourglass-loading"
+                  wrapperStyle={{}}
+                  wrapperClass=""
+                  colors={["#306cce", "#72a1ed"]}
+                />
+                <p className="text-slate-700">Please Wait...</p>
               </div>
-            ) : (
+            </div>
+          ) : (
+            <>
+              {analyticsData.length === 0 && (
+                <div className="absolute flex flex-col  justify-center sm:items-center items-end  w-full left-0 top-0 bottom-0 right-0 m-auto">
+                  <h1 className=" text-slate-800 font-serif sm:text-2xl text-[15px] font-bold mb-1">
+                    No Data For This Time Period
+                  </h1>
+                  <h3 className="sm:w-96 w-[90%] sm:ml-0 pl-6 text-center sm:text-lg text-[12px] text-slate-600 ">
+                    Share your short link to view where your engagements are
+                    coming from
+                  </h3>
+                </div>
+              )}
               <Graph graphData={analyticsData} />
-            )}
-          </>
-        )}
-      </div>
+            </>
+          )}
+        </div>
+      </React.Fragment>
     </div>
   );
 };
