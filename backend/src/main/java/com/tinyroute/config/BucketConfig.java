0fca7528c445c94b2f4bee51042b5f587d4b95ac
Sudhanshu Singh <sudhanshus026@gmail.com>
2026-05-20 20:41:23 +0530

    refactor: separate Redis and Bucket4j configuration

--- backend/src/main/java/com/tinyroute/config/BucketConfig.java
+++ backend/src/main/java/com/tinyroute/config/BucketConfig.java
@@ -2,60 +2,20 @@ package com.tinyroute.config;
 
 import io.github.bucket4j.distributed.proxy.ProxyManager;
 import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
-import io.lettuce.core.RedisClient;
-import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
-import io.lettuce.core.codec.StringCodec;
-import io.lettuce.core.codec.ByteArrayCodec;
-import io.lettuce.core.codec.RedisCodec;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
-@RequiredArgsConstructor
 public class BucketConfig {
-
-    @Value("${spring.data.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.data.redis.port}")
-    private int redisPort;
-
-    @Value("${spring.data.redis.password:}")
-    private String redisPassword;
-
-    @Bean
-    @Bean
-    public RedisClient redisClient() {
-
-        RedisURI.Builder builder =
-                RedisURI.Builder.redis(redisHost, redisPort)
-                        .withSsl(true);
-
-        if (!redisPassword.isBlank()) {
-            builder.withPassword(redisPassword.toCharArray());
-        }
-
-        return RedisClient.create(builder.build());
-    }
-
-    @Bean
-    public StatefulRedisConnection<String, byte[]> redisConnection(
-            RedisClient redisClient
-    ) {
-
-        RedisCodec<String, byte[]> codec =
-                RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE);
-
-        return redisClient.connect(codec);
-    }
 
     @Bean
     public ProxyManager<String> proxyManager(
             StatefulRedisConnection<String, byte[]> connection
     ) {
-        return LettuceBasedProxyManager.builderFor(connection).build();
+
+        return LettuceBasedProxyManager
+                .builderFor(connection)
+                .build();
     }
 }
--- backend/src/main/java/com/tinyroute/config/RedisConfig.java
+++ backend/src/main/java/com/tinyroute/config/RedisConfig.java
@@ -1,12 +1,14 @@
 package com.tinyroute.config;
 
+import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.codec.ByteArrayCodec;
+import io.lettuce.core.codec.RedisCodec;
+import io.lettuce.core.codec.StringCodec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisPassword;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
 public class RedisConfig {
@@ -17,16 +19,50 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int redisPort;
 
+    @Value("${spring.data.redis.username:}")
+    private String redisUsername;
+
     @Value("${spring.data.redis.password:}")
     private String redisPassword;
 
+    @Value("${spring.data.redis.ssl.enabled:false}")
+    private boolean redisSsl;
+
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration config =
-                new RedisStandaloneConfiguration(redisHost, redisPort);
+    public RedisClient redisClient() {
+
+        RedisURI.Builder builder = RedisURI.Builder
+                .redis(redisHost, redisPort)
+                .withSsl(redisSsl);
+
         if (!redisPassword.isBlank()) {
-            config.setPassword(RedisPassword.of(redisPassword));
+
+            if (!redisUsername.isBlank()) {
+                builder.withAuthentication(
+                        redisUsername,
+                        redisPassword.toCharArray()
+                );
+            } else {
+                builder.withPassword(
+                        redisPassword.toCharArray()
+                );
+            }
         }
-        return new LettuceConnectionFactory(config);
+
+        return RedisClient.create(builder.build());
+    }
+
+    @Bean
+    public StatefulRedisConnection<String, byte[]> redisConnection(
+            RedisClient redisClient
+    ) {
+
+        RedisCodec<String, byte[]> codec =
+                RedisCodec.of(
+                        StringCodec.UTF8,
+                        ByteArrayCodec.INSTANCE
+                );
+
+        return redisClient.connect(codec);
     }
 }
