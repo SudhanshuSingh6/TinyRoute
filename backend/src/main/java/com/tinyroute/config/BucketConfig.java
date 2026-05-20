3f7406a2dd019873505c93be13c89e8a6b92bdec
Sudhanshu Singh <sudhanshus026@gmail.com>
2026-05-20 19:40:03 +0530

    fix: add authenticated Redis support for Upstash

--- backend/src/main/java/com/tinyroute/config/BucketConfig.java
+++ backend/src/main/java/com/tinyroute/config/BucketConfig.java
@@ -3,6 +3,7 @@ package com.tinyroute.config;
 import io.github.bucket4j.distributed.proxy.ProxyManager;
 import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.codec.StringCodec;
 import io.lettuce.core.codec.ByteArrayCodec;
@@ -22,15 +23,26 @@ public class BucketConfig {
     @Value("${spring.data.redis.port}")
     private int redisPort;
 
+    @Value("${spring.data.redis.password:}")
+    private String redisPassword;
+
     @Bean
     public RedisClient redisClient() {
-        return RedisClient.create(
-                "redis://" + redisHost + ":" + redisPort
-        );
+
+        RedisURI.Builder builder =
+                RedisURI.Builder.redis(redisHost, redisPort);
+
+        if (!redisPassword.isBlank()) {
+            builder.withPassword(redisPassword.toCharArray());
+        }
+
+        return RedisClient.create(builder.build());
     }
 
     @Bean
-    public StatefulRedisConnection<String, byte[]> redisConnection(RedisClient redisClient) {
+    public StatefulRedisConnection<String, byte[]> redisConnection(
+            RedisClient redisClient
+    ) {
 
         RedisCodec<String, byte[]> codec =
                 RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE);
@@ -39,7 +51,9 @@ public class BucketConfig {
     }
 
     @Bean
-    public ProxyManager<String> proxyManager(StatefulRedisConnection<String, byte[]> connection) {
+    public ProxyManager<String> proxyManager(
+            StatefulRedisConnection<String, byte[]> connection
+    ) {
         return LettuceBasedProxyManager.builderFor(connection).build();
     }
 }
